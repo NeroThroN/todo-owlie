ebacfa7982c30a9287323c27c4f75150f2f56241
Stephen Caille <stephen.caille@gmail.com>
2020-09-24 14:29:47 +0200

    [+] Drawer component

---
+++ src/components/layout/drawer.vue
@@ -0,0 +1,18 @@
+<template>
+    <q-drawer v-model="drawerOpen">
+      Drawer Content
+    </q-drawer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawerOpen: false
+  }),
+  created () {
+    this.$root.$on('openCloseDrawer', (open) => {
+      this.drawerOpen = open
+    })
+  }
+}
+</script>
