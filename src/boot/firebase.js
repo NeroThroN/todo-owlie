acb56523542ff8384a898e3e9eefd3a2a2a9e7ef
Stephen Caille <stephen.caille@gmail.com>
2020-09-25 09:00:34 +0200

    [+] Create user document in Firestore when signUP

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -2,8 +2,14 @@ import * as firebase from 'firebase/app'
 import Credentials from './../config/credentials'
 
 export default ({ store }) => {
+  console.log('FIREBASE BOOT')
   firebase.initializeApp(Credentials.firebaseConfig)
-  firebase.auth().onAuthStateChanged((user) => {
-    store.commit('user/setUserEmail', user !== null ? user.email : null)
+  firebase.auth().onAuthStateChanged((firebaseUser) => {
+    var user = { uid: null, email: null }
+    if (firebaseUser) {
+      user.uid = firebaseUser.uid
+      user.email = firebaseUser.email
+    }
+    store.commit('user/setUser', user)
   })
 }
